d34faa2e9475a42ccea7a98720e6797fa2c0ab63
Shan <shan@leadfabric.com>
2017-06-15 19:32:25 +0530

    node -v

--- node/app.js
+++ node/app.js
@@ -1,9 +1,13 @@
 var http = require("http");
+var module1 = require("./module1");
 
 http.createServer(function (request, response) {
 
     response.writeHead(200, {"Content-Type": "text/HTML"});
     response.write("Hello World");
-    console.log("Listening to port locahost:8000");
+
+    response.write(module1.test());
+
+    console.log("Listening to port localhost:8000");
 
 }).listen(8000);
