b79c04b716117a1efb88768d624968a12ce9ca96
Shan <shan@leadfabric.com>
2017-07-11 15:46:51 +0530

    composer updates

--- app/curl/vendor/composer/autoload_static.php
+++ app/curl/vendor/composer/autoload_static.php
@@ -63,11 +63,23 @@ class ComposerStaticInit2ec7dd33b362fae6dc982cd5bb6d141a
         ),
     );
 
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit2ec7dd33b362fae6dc982cd5bb6d141a::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit2ec7dd33b362fae6dc982cd5bb6d141a::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit2ec7dd33b362fae6dc982cd5bb6d141a::$classMap;
 
         }, null, ClassLoader::class);
     }
